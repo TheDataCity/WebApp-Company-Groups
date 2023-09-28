16ff85f8a8acd6845833f59c060271eebcff3f38
Jack Lewis <44296337+Jack-Lewis1@users.noreply.github.com>
2023-09-28 12:35:16 +0100

    Changes ready for deploy

--- app.py
+++ app.py
@@ -1,116 +1,86 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 import pandas as pd
-
-import sqlite3 as SQL
-
-# from werkzeug import secure_filename
-from werkzeug.utils import secure_filename, send_file
-
+import sqlite3
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-ALLOWED_EXTENSIONS = set(["csv"])
+ALLOWED_EXTENSIONS = {"xlsx"}
+DB_NAME = 'sqlite/Use_Case.db'
+UPLOAD_FOLDER = 'uploads'
 
 
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+def execute_sql_file(file_path, conn):
+    with open(file_path, 'r') as sql_file:
+        sql_script = sql_file.read()
+    conn.executescript(sql_script)
+    conn.commit()
+
+
 @app.route("/")
-def upload_file():
+def index():
     return render_template("index.html")
 
 
-@app.route("/uploader", methods=["GET", "POST"])
-def upload_file1():
-    if request.method == "POST":
-        files = request.files.getlist("file")
+def save_output_to_excel(output, writer):
+    output.to_excel(writer, sheet_name="Sheet1", index=False)
+    wb = writer.book
+    ws = writer.sheets['Sheet1']
 
-        if len(files) > 1:
-            error = 'You are only allowed to upload one file.'
-            return render_template("index.html", error = error)
-        
-        else:
-            for file in files:
-                if file and allowed_file(file.filename):
-                    file_name = secure_filename(file.filename)                
+    (max_row, max_col) = output.shape
+    ws.autofilter(0, 0, max_row, max_col - 1)
+    ws.filter_column(max_col - 1, "Exclude_Or_Not == 0")
 
-                    # Initialise the connection to the db and make a cursor to execute queries
-                    db = SQL.connect('Use_Case.db')
-                    
-                    # Initialise cursor
-                    cursor = db.cursor()
-                    
-                    # Run pragma files for efficient access to dB
-                    with open('SQL QUERIES/pragma runs test.sql', 'r') as sql_file:
-                        sql_script = sql_file.read()
+    for row_num in output.index[(output["Exclude_Or_Not"] != 0)].tolist():
+        ws.set_row(row_num + 1, options={"hidden": True})
 
-                    cursor.executescript(sql_script)
-                    db.commit()
+    ws.set_column(first_col=0, last_col=50, width=20)
+    ws.set_column(2, 3, options={"hidden": True})
+    ws.set_column(6, 9, options={"hidden": True})
+    ws.set_column(11, 40, options={"hidden": True})
 
-                    #Read csv
-                    Use_Case = pd.read_csv(file)
-
-                    #Load file in sql dB
-                    Use_Case.to_sql("Use_Case",db,if_exists="replace",index=False)
-
-                    # Run the procedures to get the related company structure data
-                    with open('SQL QUERIES/Use_Case_Procedure_1.sql', 'r') as sql_file:
-                        sql_script = sql_file.read()
-
-                    cursor.executescript(sql_script)
-                    db.commit()
-
-                    with open('SQL QUERIES/Use_Case_Procedure_2.sql', 'r') as sql_file:
-                        sql_script = sql_file.read()
-
-                    output = pd.read_sql(sql_script,db)
-                    db.commit()
-
-                    #Drop temporary tables from dB
-                    sql_script = '''
-                    drop table if exists Use_Case;
-                    drop table if exists Use_Case_Merge;'''
-
-                    cursor.executescript(sql_script)
-                    db.commit()
-
-                    #Close connection to db
-                    db.close()
-
-                    # Initiate a writer for excel file
-                    writer = pd.ExcelWriter("output.xlsx", engine = 'xlsxwriter')
+    writer.close()
 
 
-                    # Saving dataframe to excel file
-                    output.to_excel(writer,sheet_name="Sheet1",index=False)
-                    
-                    # Steps to apply filter on excel file
-                    wb = writer.book
-                    ws = writer.sheets['Sheet1']
-                    
-                    (max_row, max_col) = output.shape
+@app.route("/uploader", methods=["POST"])
+def upload():
+    files = request.files.getlist("file")
+    if len(files) != 1:
+        return render_template("index.html", error='You are only allowed to upload one file.')
 
-                    ws.autofilter(0, 0, max_row, max_col - 1)
+    file = files[0]
+    if file and allowed_file(file.filename):
+        file_name = secure_filename(file.filename)
 
-                    ws.filter_column(max_col - 1, "Exclude_Or_Not == 0")
+        use_case = pd.read_excel(
+            file,
+            sheet_name="Companies as columns (basic)",
+            engine="openpyxl",
+            dtype={"Companynumber": str, "SICs": str},
+        )
 
-                    # Steps to hide not relevant rows
-                    for row_num in output.index[(output["Exclude_Or_Not"] != 0)].tolist():
-                        ws.set_row(row_num + 1, options={"hidden": True})
+        with sqlite3.connect(DB_NAME) as conn:
+            execute_sql_file('SQL QUERIES/pragma runs test.sql', conn)
+            use_case.to_sql("Use_Case", conn, if_exists="replace", index=False)
+            execute_sql_file('SQL QUERIES/Use_Case_Procedure_1.sql', conn)
 
-                    # close the excel write
+            output_query = open('SQL QUERIES/Use_Case_Procedure_2.sql', 'r').read()
+            output = pd.read_sql(output_query, conn)
 
-                    writer.close()                 
+            drop_query = '''
+            drop table if exists Use_Case;
+            drop table if exists Use_Case_Merge;'''
+            conn.executescript(drop_query)
+            conn.commit()
 
-                    
+        writer = pd.ExcelWriter("output/output.xlsx", engine='xlsxwriter')
+        save_output_to_excel(output, writer)
 
-        response = send_file("output.xlsx",environ=request.environ)
-        
-
-        return response
-
-
+    return send_file("output.xlsx", as_attachment=True)
 
 
 if __name__ == "__main__":
